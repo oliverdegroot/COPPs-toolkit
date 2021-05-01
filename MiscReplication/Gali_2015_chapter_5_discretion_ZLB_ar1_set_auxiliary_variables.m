ce67dd12c5b588b40f90cbc627165e29f8b2945d
mazelis <57214873+mazelis@users.noreply.github.com>
2021-05-01 17:15:15 +0200

    Delete MiscReplication directory

--- MiscReplication/Gali_2015_chapter_5_discretion_ZLB_ar1_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = Gali_2015_chapter_5_discretion_ZLB_ar1_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
